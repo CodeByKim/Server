678102b6f04343fa72e3ae78961578dd14a562b9
Garam Kim <codebykim@naver.com>
2023-11-09 01:29:57 +0900

    json config

--- Server/GameServer.cs
+++ Server/GameServer.cs
@@ -4,6 +4,15 @@ using System.Threading.Tasks;
 
 using Core.Server;
 
+using System.Text.Json;
+using System.IO;
+
+internal class ServerConfig
+{
+    public int PortNumber { get; set; }
+    public int Backlog { get; set; }
+}
+
 internal class GameServer : Server
 {
     public override void Initialize()
@@ -11,5 +20,11 @@ internal class GameServer : Server
         base.Initialize();
 
         Console.WriteLine("initialize server...");
+
+        var rawConfig = File.ReadAllText("config.json");
+        var config = JsonSerializer.Deserialize<ServerConfig>(rawConfig);
+
+        Console.WriteLine(config.PortNumber);
+        Console.WriteLine(config.Backlog);
     }
 }
